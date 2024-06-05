cbda9105e007014dba70035e378401177ecd04fc
4rn4u <vilx00@protonmail.com>
2024-06-05 21:22:27 +0200

    feature: resizing

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 
 function selectNumDiv(){
     let numDiv = document.querySelector("size");
-    let selectedValue = numDiv.value
+    let selectedValue = numDiv.value;
+    switch (selectedValue){
+        case "16x16":
+            return 16;
+        case "32x32":
+            return 32;
+        case "64x64":
+            return 64;
+    }
+}
 
+let drawButton = document.querySelector("draw");
+
+drawButton.addEventListener("click", () => {
+    drawDivs(selectNumDiv());
+
+});
+
+function drawDivs(num){
+    for(let i = 0; i<num; i++){
+        for(let j = 0; j<num; j++){
+
+        }
+    }
 }
